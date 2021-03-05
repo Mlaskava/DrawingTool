ff6c1e6bc7ff1d9452e1b0441ecb747b856c98fc
Maciej Mlaskawa <maciej.mlaskawa@gmail.com>
2021-03-05 20:55:18 +0100

    Implemented basic window, and drawing pencil-styled line

--- DrawingWindow/src/DrawingWindow.java
+++ DrawingWindow/src/DrawingWindow.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class DrawingWindow {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class DrawingWindow extends Application {
+
+    private static DrawingWindow drawingWindowInstance;
+
+
+    public DrawingWindow(){
+        drawingWindowInstance = this;
+    }
+
+
+    public static DrawingWindow getDrawingWindowInstance(){
+        return drawingWindowInstance;
+    }
+
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("DrawingWindow.fxml"));
+        Pane pane = loader.load();
+
+        stage.setScene(new Scene(pane));
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
 }
--- DrawingWindow/src/DrawingWindowController.java
+++ DrawingWindow/src/DrawingWindowController.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class DrawingWindowController {
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
+
+
+public class DrawingWindowController {
+
+    @FXML
+    private Canvas canvas;
+
+    private double xStartPosition;
+    private double yStartPosition;
+
+    EventHandler<MouseEvent> mousePressedHandler = new EventHandler<>() {
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            xStartPosition = mouseEvent.getSceneX();
+            yStartPosition = mouseEvent.getSceneY() - 50;
+        }
+    };
+
+    EventHandler<MouseEvent> mouseDraggedHandler = new EventHandler<>() {
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            double xEndPosition = mouseEvent.getSceneX();
+            double yEndPosition = mouseEvent.getSceneY() - 50;
+            canvas.getGraphicsContext2D().strokeLine(xStartPosition, yStartPosition, xEndPosition, yEndPosition);
+            xStartPosition = xEndPosition;
+            yStartPosition = yEndPosition;
+        }
+    };
+
+    @FXML
+    void initialize() {
+        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, mousePressedHandler);
+        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, mouseDraggedHandler);
+
+    }
 }
