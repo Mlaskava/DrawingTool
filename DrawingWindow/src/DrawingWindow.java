ae229b7a6932c39d0d64e0cff31f85edf033f8ee
Maciej Mlaskawa <maciej.mlaskawa@gmail.com>
2021-03-06 13:51:24 +0100

    Implemented pencil color change and cursor showing current drawing color

--- DrawingWindow/src/DrawingWindow.java
+++ DrawingWindow/src/DrawingWindow.java
@@ -6,28 +6,16 @@ import javafx.stage.Stage;
 
 public class DrawingWindow extends Application {
 
-    private static DrawingWindow drawingWindowInstance;
-
-
-    public DrawingWindow(){
-        drawingWindowInstance = this;
-    }
-
-
-    public static DrawingWindow getDrawingWindowInstance(){
-        return drawingWindowInstance;
-    }
-
-
 
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(this.getClass().getResource("DrawingWindow.fxml"));
         Pane pane = loader.load();
-
+        stage.setTitle("Drawing Application");
         stage.setScene(new Scene(pane));
         stage.show();
+
     }
 
     public static void main(String[] args){
--- DrawingWindow/src/DrawingWindowController.java
+++ DrawingWindow/src/DrawingWindowController.java
@@ -1,7 +1,11 @@
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 
 
 public class DrawingWindowController {
@@ -9,32 +13,79 @@ public class DrawingWindowController {
     @FXML
     private Canvas canvas;
 
+    @FXML
+    private Pane mainPane;
+
+
+    @FXML
+    private ColorPicker colorPicker;
+
+    @FXML
+    private Pane propertiesPane;
+
     private double xStartPosition;
     private double yStartPosition;
 
-    EventHandler<MouseEvent> mousePressedHandler = new EventHandler<>() {
-        @Override
-        public void handle(MouseEvent mouseEvent) {
-            xStartPosition = mouseEvent.getSceneX();
-            yStartPosition = mouseEvent.getSceneY() - 50;
-        }
+
+    private Circle cursor;
+
+
+    EventHandler<MouseEvent> mousePressedHandler = mouseEvent -> {
+
+        canvas.getGraphicsContext2D().setStroke(colorPicker.getValue());
+        xStartPosition = mouseEvent.getX();
+        yStartPosition = mouseEvent.getY() - 80;
+        canvas.getGraphicsContext2D().strokeOval(xStartPosition, yStartPosition, 1, 1);
     };
 
-    EventHandler<MouseEvent> mouseDraggedHandler = new EventHandler<>() {
-        @Override
-        public void handle(MouseEvent mouseEvent) {
-            double xEndPosition = mouseEvent.getSceneX();
-            double yEndPosition = mouseEvent.getSceneY() - 50;
-            canvas.getGraphicsContext2D().strokeLine(xStartPosition, yStartPosition, xEndPosition, yEndPosition);
-            xStartPosition = xEndPosition;
-            yStartPosition = yEndPosition;
-        }
+    EventHandler<MouseEvent> mouseDraggedHandler = mouseEvent -> {
+
+        moveCursor(cursor, mouseEvent);
+        double xEndPosition = mouseEvent.getX();
+        double yEndPosition = mouseEvent.getY() - 80;
+        canvas.getGraphicsContext2D().strokeLine(xStartPosition, yStartPosition, xEndPosition, yEndPosition);
+        xStartPosition = xEndPosition;
+        yStartPosition = yEndPosition;
     };
+
+    EventHandler<MouseEvent> mouseEnteredInCanvasHandler = mouseEvent -> {
+
+        cursor.setOpacity(1);
+        mainPane.setCursor(Cursor.NONE);
+        moveCursor(cursor, mouseEvent);
+    };
+
+
+    EventHandler<MouseEvent> mouseMovedHandler = mouseEvent -> moveCursor(cursor, mouseEvent);
+
+    EventHandler<MouseEvent> mouseEnteredInPropertiesPaneHandler = mouseEvent -> {
+        propertiesPane.setCursor(Cursor.DEFAULT);
+        cursor.setOpacity(0);
+    };
+
+    void moveCursor(Circle cursor, MouseEvent mouseEvent) {
+
+        cursor.setStroke(colorPicker.getValue());
+        cursor.setFill(colorPicker.getValue());
+        cursor.setCenterX(mouseEvent.getX());
+        cursor.setCenterY(mouseEvent.getSceneY());
+    }
 
     @FXML
     void initialize() {
-        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, mousePressedHandler);
-        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, mouseDraggedHandler);
+        cursor = new Circle(0.5, colorPicker.getValue());
+        cursor.setOpacity(0);
+
+        mainPane.getChildren().add(cursor);
+
+        propertiesPane.addEventHandler(MouseEvent.MOUSE_ENTERED, mouseEnteredInPropertiesPaneHandler);
+
+        cursor.addEventHandler(MouseEvent.MOUSE_PRESSED, mousePressedHandler);
+        cursor.addEventHandler(MouseEvent.MOUSE_DRAGGED, mouseDraggedHandler);
+
+        canvas.addEventHandler(MouseEvent.MOUSE_ENTERED, mouseEnteredInCanvasHandler);
+        canvas.addEventHandler(MouseEvent.MOUSE_MOVED, mouseMovedHandler);
+
 
     }
 }
